e61766758dd396255894197ce892fa7bd4aa76a3
李钊文 <lizhaowen@jwzg.com>
2023-11-17 19:24:14 +0800

    Refactor generateTemplateDir function to accept options object and return the rootDir

--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
 import fs from 'fs';
 import path from 'path';
+import shell from 'shelljs';
+import { BuildUOSType, TemplateDirType, DesktopEntryType, controlFileType } from './types';
+import { rules, install } from './replaceFilesInfo';
 
 /**
  * Create the following tree structure files and directories
@@ -18,24 +21,19 @@ import path from 'path';
             └── info // Desktop Info File
  */
 
-type TemplateDirType = {
-  appleId: string;
-  svgPath?: string;
-  desktopEntryFileContent: string;
-  desktopInfoFileContent: string;
-}
-
 const TemplateDirTypeDefault: TemplateDirType = {
   appleId: 'com.example.app',
   svgPath: '',
   desktopEntryFileContent: `[Desktop Entry]`,
-  desktopInfoFileContent: `{}`,
+  unpackedDir: '',
+  // desktopInfoFileContent: `{}`,
 }
 
-function generateTemplateDir(options: TemplateDirType = TemplateDirTypeDefault) {
+function generateTemplateDir(options: TemplateDirType = TemplateDirTypeDefault): string {
   const { appleId, svgPath, desktopEntryFileContent, desktopInfoFileContent } = options;
 
   const currentDir = process.cwd();
+  // todo: template 要换成最后包的名称
   const rootDir = path.join(currentDir, 'template', `opt/apps/${appleId}`);
   const entriesDir = `${rootDir}/entries/applications`;
   const iconsDir = `${entriesDir}/icons/hicolor/scalable/apps`;
@@ -60,7 +58,7 @@ function generateTemplateDir(options: TemplateDirType = TemplateDirTypeDefault) 
 
   if (!fs.existsSync(desktopInfoFile)) {
     console.info('Creating file: ', desktopInfoFile);
-    fs.writeFileSync(desktopInfoFile, desktopInfoFileContent);
+    fs.writeFileSync(desktopInfoFile, JSON.stringify(desktopInfoFileContent || {}, null, 2));
   }
   
   // 如果有 svgPath，就复制 svg 文件到 iconsDir，并且重名为 ${appleId}.svg，如果没有 svgPath，就创建空的 svg 文件
@@ -73,17 +71,135 @@ function generateTemplateDir(options: TemplateDirType = TemplateDirTypeDefault) 
       fs.writeFileSync(svgFile, '');
     }
   }
+
+  // unpackedDir 复制并且替换files目录，目录名保留
+  if (fs.existsSync(filesDir)) {
+    fs.rmSync(filesDir, { recursive: true });
+  }
+  
+  console.info('Copying directory: ', filesDir);
+  shell.cp('-R', options.unpackedDir, filesDir);
+
+  // 返回当前模板目录
+  return rootDir;
 }
-function removeTemplateDir() {
-  const currentDir = process.cwd();
-  const rootDir = path.join(currentDir, 'template');
+function removeTemplateDir(rootDir: string) {
+  // const currentDir = process.cwd();
+  // const rootDir = path.join(currentDir, 'template');
   if (fs.existsSync(rootDir)) {
     fs.rmSync(rootDir, { recursive: true });
   }
 }
 
-removeTemplateDir();
-generateTemplateDir();
-// setTimeout(() => {
-//   removeTemplateDir();
-// }, 2000);
+function desktopEntryToString(options: DesktopEntryType) {
+  const { Categories, Name, GenericName, Type, Exec, Icon, MimeTypes } = options;
+  return `[Desktop Entry]
+Categories=${Categories}
+Name=${Name}
+GenericName=${GenericName}
+Type=${Type}
+Exec=${Exec}
+Icon=${Icon}
+MimeTypes=${MimeTypes}
+`;
+}
+
+export async function buildUOS(options: BuildUOSType) {
+  const { svgPath, appleId, unpackedDir, DesktopEntry, DesktopInfo, controlFile } = options;
+  
+  // 1. 创建模板目录
+  if (options.beforeGenerateTemplateDir) {
+    options.beforeGenerateTemplateDir();
+  }
+
+  const rootDir = generateTemplateDir({
+    appleId,
+    svgPath,
+    unpackedDir,
+    desktopEntryFileContent: desktopEntryToString(DesktopEntry),
+    desktopInfoFileContent: DesktopInfo,
+  });
+
+  if (options.afterGenerateTemplateDir) {
+    options.afterGenerateTemplateDir(rootDir);
+  }
+
+  // 2. 打包
+  if (options.beforePack) {
+    options.beforePack();
+  }
+
+  pack(rootDir, controlFile);
+
+  if (options.afterPack) {
+    options.afterPack();
+  }
+
+  // 3. 删除模板目录
+  if (options.beforeRemoveTemplateDir) {
+    options.beforeRemoveTemplateDir();
+  }
+
+  removeTemplateDir(rootDir);
+
+  if (options.afterRemoveTemplateDir) {
+    options.afterRemoveTemplateDir();
+  }
+}
+
+// 打包
+function pack(rootDir: string, controlFileInfo: controlFileType) {
+  console.info('start pack');
+  shell.exec(`cd ${rootDir} && USER=root dh_make --createorig -s -n -y`)
+  
+  const debianDir = path.join(rootDir, 'debian');
+  console.info('debianDir: ', debianDir);
+
+  shell.exec(`cd ${debianDir} && rm *.ex *.EX`)
+	shell.exec(`cd ${debianDir} && rm -rf *.docs README README.* `)
+	shell.exec(`cd ${debianDir} && rm control rules`)
+	// shell.exec(`cd ${debianDir} && ls -al`)
+
+  // 3. 替换文件
+  // 3.1 替换 control 文件
+  const controlFile = path.join(debianDir, 'control');
+  console.info('controlFile: ', controlFile);
+  if (fs.existsSync(controlFile)) {
+    fs.rmSync(controlFile);
+  }
+  fs.writeFileSync(controlFile, controlFileToString(controlFileInfo));
+
+  // 3.2 替换 rules 文件
+  const rulesFile = path.join(debianDir, 'rules');
+  console.info('rulesFile: ', rulesFile);
+  if (fs.existsSync(rulesFile)) {
+    fs.rmSync(rulesFile);
+  }
+  fs.writeFileSync(rulesFile, rules);
+
+  // 3.3 替换 install 文件
+  const installFile = path.join(debianDir, 'install');
+  console.info('installFile: ', installFile);
+  if (fs.existsSync(installFile)) {
+    fs.rmSync(installFile);
+  }
+  fs.writeFileSync(installFile, install);
+
+  // 4. 打包
+  shell.exec(`cd ${rootDir} && fakeroot dpkg-buildpackage -b -us -uc`);
+}
+
+function controlFileToString(options: controlFileType) {
+  const { Source, Section, Priority, Maintainer, BuildDepends, StandardsVersion, Homepage, Package, Architecture } = options;
+  return `Source: ${Source}
+Section: ${Section}
+Priority: ${Priority}
+Maintainer: ${Maintainer}
+Build-Depends: ${BuildDepends}
+Standards-Version: ${StandardsVersion}
+Homepage: ${Homepage}
+Package: ${Package}
+Architecture: ${Architecture}
+Description: ${Package}
+`;
+}
---
+++ types.d.ts
@@ -0,0 +1,71 @@
+
+type DesktopInfoPermissionType = {
+  autostart: boolean;
+  notification: boolean;
+  trayicon: boolean;
+  clipboard: boolean;
+  account: boolean;
+  bluetooth: boolean;
+  camera: boolean;
+  audio_record: boolean;
+  installed_apps: boolean;
+}
+
+type DesktopInfoType = {
+  appleId: string;
+  name: string;
+  version: string;
+  description: string;
+  permissions: DesktopInfoPermissionType;
+}
+
+export type TemplateDirType = {
+  appleId: string;
+  svgPath?: string;
+  desktopEntryFileContent: string;
+  desktopInfoFileContent?: DesktopInfoType;
+  unpackedDir: string;
+}
+
+export type DesktopEntryType = {
+  Categories: string;
+  Name: string;
+  GenericName: string;
+  Type: string;
+  Exec: string;
+  Icon: string;
+  MimeTypes: string;
+  // Comment: string;
+  // Terminal: string;
+  // StartupNotify: string;
+}
+
+export type BuildUOSType = {
+  svgPath: string; // svg 文件路径, app icon
+  appleId: string; // app id
+  unpackedDir: string; // 解压后的文件夹路径
+  DesktopInfo: DesktopInfoType;
+  DesktopEntry: DesktopEntryType;
+  controlFile: controlFileType;
+  // lifecycle
+  beforeGenerateTemplateDir?: () => void;
+  afterGenerateTemplateDir?: (templatePath: string) => void;
+  beforePack?: () => void;
+  afterPack?: () => void;
+  beforeRemoveTemplateDir?: () => void;
+  afterRemoveTemplateDir?: () => void;
+}
+
+export type controlFileType = {
+  Source: string;
+  Section: string;
+  Priority: string;
+  Maintainer: string;
+  BuildDepends: string;
+  StandardsVersion: string;
+  Homepage: string;
+  Package: string;
+  Architecture: string;
+  // Depends: string;
+  Description: string;
+}
